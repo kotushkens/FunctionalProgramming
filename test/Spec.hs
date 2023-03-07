4c4da699ac73ec91cc145f47181641807a2f63c8
Ekaterina Gorbunova <284357@niuitmo.ru>
2023-03-07 10:42:21 +0300

    fix test

--- test/Spec.hs
+++ test/Spec.hs
@@ -8,10 +8,10 @@ lcmForListTest :: Test
 lcmForListTest = TestCase (assertEqual "lcmForListTest" 232792560 (lcmForList [1..20]))
 
 cycleLengthTest :: Test
-cycleLengthTest = TestCase (assertEqual "cycleLengthTest" 6 (cycleLength 7))
+cycleLengthTest = TestCase (assertEqual "cycleLengthTest" (7,6) (cycleLength 7))
 
 argMaxTest :: Test
-argMaxTest = TestCase (assertEqual "argMaxTest" 983 (argMax cycleLength [1..1000]))
+argMaxTest = TestCase (assertEqual "argMaxTest" 983 (snd $ argMax $ take 1000 $ mapList [1..]))
 
 tests :: Test
 tests = TestList ["smallestDividendByAllTest returns correct result" ~: smallestDividendByAllTest,
