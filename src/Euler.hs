094c07357f1c5559c9b10f996bd09ad1d2fb8c77
Ekaterina Gorbunova <284357@niuitmo.ru>
2023-03-07 13:39:25 +0300

    fix formatting

--- src/Euler.hs
+++ src/Euler.hs
@@ -1,11 +1,11 @@
-module Euler (
-      smallestDividendByAll,
-      lcmForList,
-      argMax,
-      cycleLength,
-      mapList
-    )
-  where
+module Euler
+  ( smallestDividendByAll,
+    lcmForList,
+    argMax,
+    cycleLength,
+    mapList,
+  )
+where
 
 import Data.List (elemIndex, foldl1')
 
@@ -22,20 +22,20 @@ lcmForList [] = 1
 lcmForList [x] = x
 lcmForList (x : xs) = customLcm x (lcmForList xs)
   where
-   customLcm _ 0 = 0
-   customLcm 0 _ = 0
-   customLcm a b = abs ((a `quot` gcd a b) * b)
+    customLcm _ 0 = 0
+    customLcm 0 _ = 0
+    customLcm a b = abs ((a `quot` gcd a b) * b)
 
 cycleLength :: Int -> (Int, Int)
 cycleLength n =
- let remainders = iterate (\x -> mod (x * 10) n) 1
-     findCycle (_ : _) [] = undefined
-     findCycle [] [] = undefined
-     findCycle [] (x : xs) = findCycle (x : []) xs
-     findCycle acc (x : xs) = case elemIndex x acc of
-       Nothing -> findCycle (x : acc) xs
-       Just i -> (i + 1, n)
-  in findCycle [] remainders
+  let remainders = iterate (\x -> mod (x * 10) n) 1
+      findCycle (_ : _) [] = undefined
+      findCycle [] [] = undefined
+      findCycle [] (x : xs) = findCycle (x : []) xs
+      findCycle acc (x : xs) = case elemIndex x acc of
+        Nothing -> findCycle (x : acc) xs
+        Just i -> (i + 1, n)
+   in findCycle [] remainders
 
 mapList :: [Int] -> [(Int, Int)]
 mapList ls = map cycleLength ls
