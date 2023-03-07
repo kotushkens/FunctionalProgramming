65e613c198cc4eb787cf8e1551e37c43103c117f
Ekaterina Gorbunova <284357@niuitmo.ru>
2023-03-07 13:01:01 +0300

    fix formatting

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,24 +1,27 @@
 import Test.HUnit
-import Euler
 
-smallestDividendByAllTest :: Test 
+smallestDividendByAllTest :: Test
 smallestDividendByAllTest = TestCase (assertEqual "smallestDividendByAllTest" 232792560 (smallestDividendByAll 20))
 
-lcmForListTest :: Test 
-lcmForListTest = TestCase (assertEqual "lcmForListTest" 232792560 (lcmForList [1..20]))
+lcmForListTest :: Test
+lcmForListTest = TestCase (assertEqual "lcmForListTest" 232792560 (lcmForList [1 .. 20]))
 
 cycleLengthTest :: Test
-cycleLengthTest = TestCase (assertEqual "cycleLengthTest" (7,6) (cycleLength 7))
+cycleLengthTest = TestCase (assertEqual "cycleLengthTest" (7, 6) (cycleLength 7))
 
 argMaxTest :: Test
-argMaxTest = TestCase (assertEqual "argMaxTest" 983 (snd $ argMax $ take 1000 $ mapList [1..]))
+argMaxTest = TestCase (assertEqual "argMaxTest" 983 (snd $ argMax $ take 1000 $ mapList [1 ..]))
 
 tests :: Test
-tests = TestList ["smallestDividendByAllTest returns correct result" ~: smallestDividendByAllTest,
-                  "lcmForListTest returns correct result" ~: lcmForListTest,
-                  "cycleLength returns correct result" ~: cycleLengthTest,
-                  "cycleLength argMax return is correct" ~: argMaxTest]
+tests =
+  TestList
+    [ "smallestDividendByAllTest returns correct result" ~: smallestDividendByAllTest,
+      "lcmForListTest returns correct result" ~: lcmForListTest,
+      "cycleLength returns correct result" ~: cycleLengthTest,
+      "cycleLength argMax return is correct" ~: argMaxTest
+    ]
 
 main :: IO Counts
 main = do
   runTestTT tests
+ 
