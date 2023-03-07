083c4e7e29542986228071f78b60b996819d321b
Ekaterina Gorbunova <284357@niuitmo.ru>
2023-03-07 13:40:55 +0300

    fix formatting

--- app/Main.hs
+++ app/Main.hs
@@ -12,4 +12,3 @@ main ::
     print (lcmForList list1)
     -- Find the value of d < 1000 for which 1/d contains the longest recurring cycle in its decimal fraction part.
     print (snd $ argMax $ take 1000 $ mapList [1 ..])
- 
