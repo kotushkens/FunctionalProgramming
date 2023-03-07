08daa74ee564031f2883b1753da4fab66556c20d
Ekaterina Gorbunova <284357@niuitmo.ru>
2023-03-07 14:05:38 +0300

    fix formatting

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,4 +1,5 @@
 import Test.HUnit
+import Euler
 
 smallestDividendByAllTest :: Test
 smallestDividendByAllTest = TestCase (assertEqual "smallestDividendByAllTest" 232792560 (smallestDividendByAll 20))
@@ -7,7 +8,7 @@ lcmForListTest :: Test
 lcmForListTest = TestCase (assertEqual "lcmForListTest" 232792560 (lcmForList [1 .. 20]))
 
 cycleLengthTest :: Test
-cycleLengthTest = TestCase (assertEqual "cycleLengthTest" (7, 6) (cycleLength 7))
+cycleLengthTest = TestCase (assertEqual "cycleLengthTest" (6, 7) (cycleLength 7))
 
 argMaxTest :: Test
 argMaxTest = TestCase (assertEqual "argMaxTest" 983 (snd $ argMax $ take 1000 $ mapList [1 ..]))
